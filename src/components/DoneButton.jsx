97027c0059d9799298b4009aecd5bcc7546879ed
Lalasimon <lalaszymon@gmail.com>
2023-03-11 21:35:59 +0100

    After hitting button done, li element background getting green

--- src/components/DoneButton.jsx
+++ src/components/DoneButton.jsx
@@ -3,15 +3,22 @@ import useLocalStorageState from "use-local-storage-state";
 
 const DoneButton = ({ lessonId }) => {
     const [progress, setProgress] = useLocalStorageState("progress", 0);
+    const [greenColor1_1, greenSetColor1_1] = useLocalStorageState(
+        "green1_1",
+        0
+    );
+
     const isLessonDone =
         localStorage.getItem(`progress ${lessonId} done`) === "true";
 
     const handleClick = () => {
         if (!isLessonDone) {
             localStorage.setItem(`progress ${lessonId} done`, "true");
+            greenSetColor1_1(1);
             setProgress(progress + 10);
         } else {
             localStorage.removeItem(`progress ${lessonId} done`);
+            greenSetColor1_1(0);
             setProgress(progress - 10);
         }
     };
