3f3af960a10589f2de9785c045f206e7f18c5b9a
Lalasimon <lalaszymon@gmail.com>
2023-03-15 11:23:29 +0100

    styling home site done

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router";
 import { ProgressBar } from "./ProgressBar";
 import "../index.scss";
+import "primeicons/primeicons.css";
 
 const UserProfile = () => {
     const navigate = useNavigate();
@@ -15,21 +16,17 @@ const UserProfile = () => {
         document.location.reload(true);
     };
     return (
-        <>
-            <div className="flex flex-col justify-center items-center gap-44">
-                <div>Intrudaction</div>
-                <div className="userProfile flex flex-col justify-center items-center border-2 border-solid border-yellow-900 p-10 gap-8">
-                    <h1>{email}</h1>
-                    <ProgressBar></ProgressBar>
-                    <button
-                        className="bg-yellow-900 opacity-50 text-white p-3 flex justify-center text-center text-xl focus:outline-none duration-300 rounded-xl hover:bg-yellow-700 hover:opacity-100 scale-100 hover:scale-125"
-                        onClick={handleLogoutClick}
-                    >
-                        LOG OUT
-                    </button>
-                </div>
-            </div>
-        </>
+        <div className="userProfile flex flex-col justify-center items-center border-2 border-solid border-yellow-900 p-10 gap-8 rounded-xl">
+            <i className="pi pi-user" style={{ fontSize: "3rem" }}></i>
+            <span className="text-xl">{email}</span>
+            <ProgressBar></ProgressBar>
+            <button
+                className="bg-yellow-900 opacity-50 text-white p-3 flex justify-center text-center text-xl focus:outline-none duration-300 rounded-xl hover:bg-yellow-700 hover:opacity-100 scale-100 hover:scale-125"
+                onClick={handleLogoutClick}
+            >
+                LOG OUT
+            </button>
+        </div>
     );
 };
 
