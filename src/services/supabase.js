5a97cb974073b69fdc964c4ab03da8dae125c737
Szymon Lala <lalaszymon@gmail.com>
2023-03-15 22:33:42 +0100

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const API_URL = import.meta.env.VITE_SUPABASE_URL;
-const API_KEY = import.meta.env.VITE_SUPABASE_API_KEY;
+const API_URL = VITE_PUBLIC_import.meta.env.VITE_SUPABASE_URL;
+const API_KEY = VITE_PUBLIC_import.meta.env.VITE_SUPABASE_API_KEY;
 const supabase = createClient(API_URL, API_KEY);
 export default supabase;
