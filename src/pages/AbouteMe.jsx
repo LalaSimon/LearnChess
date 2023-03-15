3a001e33c2b952d2f411aa9ea70c81a27485451d
Lalasimon <lalaszymon@gmail.com>
2023-03-15 21:35:16 +0100

    final app done

--- src/pages/AbouteMe.jsx
+++ src/pages/AbouteMe.jsx
@@ -6,37 +6,61 @@ import "primeicons/primeicons.css";
 const AboutMe = () => {
     return (
         <>
+            z
             <div className="flex flex-col w-full h-full">
+                {" "}
+                <h1 className="text-4xl border-b-2 border-solid pb-10 text-center border-yellow-900">
+                    Chess bio
+                </h1>
                 <div className="flex justify-around mt-10">
-                    <img className="me ml-5" src={myImage} />
+                    {" "}
+                    <img
+                        className="me ml-5 mt-8 shadow-lg shadow-yellow-900"
+                        src={myImage}
+                    />
                     <div className="flex flex-col justify-start items-center mx-14">
-                        <h1 className="text-4xl border-b-2 border-solid border-yellow-900">
-                            Chess bio
-                        </h1>
-                        <p className="mt-6 text-justify text-3xl">
-                            Hi! My name is Szymon and im junior frontend
-                            developer from Poland. This guy on left side its me,
-                            during one of chess tournaments that i played in my
-                            life. Chess is my biggest passion for a while and i
-                            decided to create an application where you can
-                            basically learn fundemntals and basics about chess
-                            for free. I'm really advanced chess player to be
-                            honest, there are plenty of better players than me,
-                            but for teaching basics i think its enough.
-                            Officialy i have only 1114 fide which im not proud
-                            of, because i played only one classical tournament
-                            during my journey with chess and it was in early
-                            2022. At this time (march 2023) i have 1400 elo
-                            points on Chess.com which place me in almost 4% of
-                            best players in the world. I love chess because you
-                            can develop yourself all the time and never stop,
-                            just like with being good programmer. Hope you spend
-                            time well during course on this site.
-                        </p>
+                        <div className="mt-6 text-justify text-3xl flex flex-col gap-4">
+                            <p>Hi! </p>
+
+                            <p>
+                                My name is Szymon and im junior frontend
+                                developer from Poland. This guy on left side its
+                                me, during one of chess tournaments that i
+                                played in my life.{" "}
+                            </p>
+
+                            <p>
+                                {" "}
+                                Chess is my biggest passion for a while and i
+                                decided to create an application where you can
+                                basically learn fundemntals and basics about
+                                chess for free. I'm really advanced chess player
+                                to be honest, there are plenty of better players
+                                than me, but for teaching basics i think its
+                                enough.
+                            </p>
+
+                            <p>
+                                Officialy i have only 1114 fide which im not
+                                proud of, because i played only one classical
+                                tournament during my journey with chess and it
+                                was in early 2022. At this time (march 2023) i
+                                have 1400 elo points on Chess.com which place me
+                                in almost 4% of best players in the world.{" "}
+                            </p>
+
+                            <p>
+                                {" "}
+                                I love chess because you can develop yourself
+                                all the time and never stop, just like with
+                                being good programmer. Hope you spend time well
+                                during course on this site.
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div className="flex justify-center flex-col items-center mt-10 border-t-2 border-solid border-yellow-900 pt-8">
-                    <h2 className="text-4xl">Contact links</h2>
+                <div className="flex justify-center flex-col items-center mt-14 border-t-2 border-solid border-yellow-900 pt-8">
+                    <h2 className="text-4xl mt-2">Contact links</h2>
                     <ul className="flex gap-44 justify-center items-center mt-10">
                         <a
                             href="https://github.com/LalaSimon?tab=repositories"
--- src/pages/OpenBoard.jsx
+++ src/pages/OpenBoard.jsx
@@ -1,14 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useWindowSize } from "react-use";
 import "../index.scss";
-import { PlayChess } from "../components/PlayChess";
+import { Chessboard } from "react-chessboard";
+import { Chess } from "chess.js";
 
-const OpenBoard = () => {
+const OpenBoard = ({ boardposition, boardOrientation }) => {
+    const [game, setGame] = useState(
+        boardposition ? new Chess(boardposition) : new Chess()
+    );
+    const [position, setPosition] = useState("white");
+    // sprawdza czy doszlo do zmiany pozycji na szachownicy
+    useEffect(() => {
+        setGame(new Chess(boardposition));
+    }, [boardposition]);
+
+    const makeAMove = (move) => {
+        const gameCopy = { ...game };
+        const result = gameCopy.move(move);
+        setGame(gameCopy);
+        return result; // null if the move was illegal, the move object if the move was legal
+    };
+
+    const onDrop = (sourceSquare, targetSquare) => {
+        const move = makeAMove({
+            from: sourceSquare,
+            to: targetSquare,
+            promotion: "q", // always promote to a queen for example simplicity
+        });
+    };
+    const flipBoard = () => {
+        setPosition(position === "white" ? "black" : "white");
+    };
+    const undoMove = () => {
+        game.undo();
+        setGame(new Chess(game.fen()));
+    };
+    const checkWidth = () => {
+        const { width } = useWindowSize();
+        let value = 0;
+
+        if (width < 640) {
+            value = 350;
+        } else if (width < 768) {
+            value = 400;
+        } else if (width >= 768 && width < 1024) {
+            value = 450;
+        } else {
+            value = 550;
+        }
+
+        return value;
+    };
     return (
         <>
-            <h1>OpenBoard</h1>
-            <div className="websiteBody flex flex-col justify-center items-center">
-                <PlayChess></PlayChess>
-                <button onClick={() => undoMove()}>Cofnij ruch</button>
+            <p className="text-2xl text-center mx-96 mt-14 border-b-2 border-solid border-yellow-900 p-2">
+                This is OpenBoard - An chessboard that you can play a game of
+                chess if you dont have physical one in your home. Bring your
+                opponent in front of screen and play a game. With flip button
+                you can flip checkboard. With undo button you are allowed to
+                undo one move back,
+            </p>
+            <div className="flex justify-center items-center mt-12">
+                <div className="chessboard flex flex-col mx-44 justify-center items-center shadow-lg shadow-yellow-900">
+                    <Chessboard
+                        boardWidth={checkWidth()}
+                        position={game.fen()} // Dont touch it!!! If you want a specific position, pass it to props in FEN
+                        onPieceDrop={onDrop}
+                        boardOrientation={position}
+                        customDarkSquareStyle={{ backgroundColor: "#713f12" }}
+                        customLightSquareStyle={{ backgroundColor: "#A57548" }}
+                    />
+                </div>
+                <div className="flex flex-col gap-6">
+                    <button
+                        className="bg-yellow-900 opacity-50 text-white p-4 flex justify-center text-center text-xl focus:outline-none duration-300 rounded-xl hover:bg-yellow-700 hover:opacity-100 scale-100 hover:scale-125 shadow-lg shadow-yellow-900 active:shadow-inner active:shadow-gray-700"
+                        onClick={flipBoard}
+                    >
+                        Flip the board
+                    </button>
+                    <button
+                        className="bg-yellow-900 opacity-50 text-white p-4 flex justify-center text-center text-xl focus:outline-none duration-300 rounded-xl hover:bg-yellow-700 hover:opacity-100 scale-100 hover:scale-125 shadow-lg shadow-yellow-900 active:shadow-inner active:shadow-gray-700"
+                        onClick={undoMove}
+                    >
+                        Undo move
+                    </button>
+                </div>
             </div>
         </>
     );
